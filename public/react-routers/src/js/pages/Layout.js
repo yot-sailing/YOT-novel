6e50f37a722720bd9b797b0c6e7429139aee78ed
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-05-30 00:19:12 +0900

    header & footer

--- public/react-routers/src/js/pages/Layout.js
+++ public/react-routers/src/js/pages/Layout.js
@@ -3,6 +3,7 @@ import Footer from '../components/layout/Footer';
 import Nav from '../components/layout/Nav';
 import { Link , withRouter} from 'react-router-dom';
 
+
 class Layout extends React.Component{
     // navigate() {
     //     console.log(this.props.history);
@@ -11,22 +12,21 @@ class Layout extends React.Component{
     render() {
         const { location } = this.props;
         const containterStyle = {
-            marginTop : "150px"
+            paddingTop : "100px",
+            marginBottom: "8em"
         };
         console.log("layout");
         return (
-            <div>
-                <Nav location={ location } />
-                <div class="container" style={containterStyle}>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            {this.props.children}
-                        </div>
-                    </div>
-                </div>
-                <p class="to-the-top">⬆️　to the top</p>
-                <Footer />
+          <div>
+            <div id="top-link"></div>
+            <Nav location={location} />
+            <div class="container" style={containterStyle}>
+              <div class="row">
+                <div class="col-lg-12">{this.props.children}</div>
+              </div>
             </div>
+            <Footer />
+          </div>
         );
     }
 }
