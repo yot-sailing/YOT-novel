7f90a6fd234a8b4ec970ceacba68b80304491820
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-08-15 21:52:45 +0900

    ランクのデザイン

--- public/react-routers/src/js/components/Article.js
+++ public/react-routers/src/js/components/Article.js
@@ -6,13 +6,17 @@ export default class extends React.Component{
         const { title } = this.props;
         const { author } = this.props;
         const { category } = this.props;
+        const { rank } = this.props;
         return (
             <a class="list-novel" href="#">
-                <div class="list-novel-title">{ title }</div>
-                <div class="list-novel-abst">{abstract}</div>
-                <div class="list-novel-info">
-                    <div class="list-novel-author">{ author }</div>
-                    <div class="list-novel-cat">{category}</div>
+                <div class="list-novel-rank">{ rank }</div>
+                <div class="list-novel-content">
+                    <div class="list-novel-title">{ title }</div>
+                    <div class="list-novel-abst">{ abstract }</div>
+                    <div class="list-novel-info">
+                        <div class="list-novel-author">{ author }</div>
+                        <div class="list-novel-cat">{ category }</div>
+                    </div>
                 </div>
             </a>
         );
