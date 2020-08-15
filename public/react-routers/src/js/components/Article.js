687699230eeb023496950327324c3825cbed66c8
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-08-15 20:38:10 +0900

    リスト内の記事デザイン変更

--- public/react-routers/src/js/components/Article.js
+++ public/react-routers/src/js/components/Article.js
@@ -8,12 +8,12 @@ export default class extends React.Component{
         const { category } = this.props;
         return (
             <a class="list-novel" href="#">
-                    <div class="list-novel-title">{ title }</div>
-                    <div class="list-novel-abst">{abstract}</div>
-                    <div class="list-novel-info">
-                        <div class="list-novel-author">{ author }</div>
-                        <div class="list-novel-cat">{category}</div>
-                    </div>
+                <div class="list-novel-title">{ title }</div>
+                <div class="list-novel-abst">{abstract}</div>
+                <div class="list-novel-info">
+                    <div class="list-novel-author">{ author }</div>
+                    <div class="list-novel-cat">{category}</div>
+                </div>
             </a>
         );
     }
