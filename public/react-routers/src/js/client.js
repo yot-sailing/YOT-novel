dfa3bad294c050720a0fd92b97cf439248242a4f
tkt914 <cyumon_tkt.0914@icloud.com>
2020-08-16 01:28:15 +0900

    404page

--- public/react-routers/src/js/client.js
+++ public/react-routers/src/js/client.js
@@ -11,6 +11,7 @@ import Ranking from './pages/Ranking';
 import createNovel from './pages/createNovel';
 import SignIn from './pages/signIn';
 import SignUp from './pages/signUp';
+import NotFound from './pages/404'
 
 import Auth from './auth/Auth';
 
@@ -19,20 +20,23 @@ const app = document.getElementById('app');
 ReactDOM.render(
     <Router>
         <Layout>
-            <Route exact path="/" component={Top}></Route>
-            <Route path="/ranking" component={Ranking}></Route>
-            <Route path="/search/" component={Search}></Route>
-            <Route path="/search/:mode(main|extra)" component={Search}></Route>
-            <Route path="/createNovel" component={createNovel}></Route>
-            <Route path="/signIn" component={SignIn}></Route>
-            <Route path="/signUp" component={SignUp}></Route>
-            <Auth>
-                <Switch>
-                    <Route path="/bookmarks/" component={Bookmarks}></Route>
-                    <Route path="/history/" component={History}></Route>
-                    <Route path="/mypage/" component={MyPage}></Route>
-                </Switch>
-            </Auth>
+            <Switch>
+                <Route exact path="/" component={Top}></Route>
+                <Route path="/ranking" component={Ranking}></Route>
+                <Route path="/search/" component={Search}></Route>
+                <Route path="/search/:mode(main|extra)" component={Search}></Route>
+                <Route path="/createNovel" component={createNovel}></Route>
+                <Route path="/signIn" component={SignIn}></Route>
+                <Route path="/signUp" component={SignUp}></Route>
+                <Auth>
+                    <Switch>
+                        <Route path="/bookmarks/" component={Bookmarks}></Route>
+                        <Route path="/history/" component={History}></Route>
+                        <Route path="/mypage/" component={MyPage}></Route>
+                    </Switch>
+                </Auth>
+                <Route component={NotFound} />
+            </Switch>
         </Layout>
     </Router>
     , app
