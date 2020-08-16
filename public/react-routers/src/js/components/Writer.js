44fe4c7adfd3a8c418bd33ca8ba25592e9fddcf5
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-08-16 11:56:57 +0900

    マイページお気に入りユーザーデザイン

--- public/react-routers/src/js/components/Writer.js
+++ public/react-routers/src/js/components/Writer.js
@@ -4,11 +4,9 @@ export default class extends React.Component{
     render() {
         const { username } = this.props;
         return (
-            <div class="col-md-4">
-                <h4>{ username }</h4>
-                <p>I am a kyoto univ student.</p>
-                <a class="btn btn-default" href="#">More Info</a>
-            </div>
+            <button class="list-writer" onClick={this.handleClick}>
+                <div class="list-writer-name" >{ username }</div>
+            </button>
         );
     }
 }
