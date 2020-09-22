d778940f923f26d599da0198d2c7a285775adb2b
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-09-22 15:36:46 +0900

    delete commentout and log in Layout

--- public/react-routers/src/js/pages/Layout.js
+++ public/react-routers/src/js/pages/Layout.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import Footer from '../components/layout/Footer';
 import Nav from '../components/layout/Nav';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class Layout extends React.Component {
-  // navigate() {
-  //     console.log(this.props.history);
-  //     this.props.history.push("/");
-  // }
   render() {
     const { location } = this.props;
     const containterStyle = {
       paddingTop: '100px',
       marginBottom: '8em',
     };
-    console.log('layout');
+
     return (
       <div>
         <div id="top-link"></div>
