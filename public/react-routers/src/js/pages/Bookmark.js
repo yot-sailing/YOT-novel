e04933915cc0e47002852f027d38b33729953852
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-09-22 15:00:42 +0900

    change state name and add comments in Bookmark

--- public/react-routers/src/js/pages/Bookmark.js
+++ public/react-routers/src/js/pages/Bookmark.js
@@ -8,60 +8,54 @@ export default class extends React.Component {
     super(props);
     this.state = {
       collapsed: true,
-      list: [],
+      favoriteNovels: [],
     };
 
     var user = firebase.auth().currentUser;
-    var email = user.email;
-    //var uid = user.uid;
     var user_doc_id = [];
-    console.log('今ログインしてる人のemailは', email);
     db.collection('users')
-      .where('email', '==', email)
+      .where('email', '==', user.email)
       .get()
-      // .then(doc => {
-      //   // doc.data() is never undefined for query doc snapshots
-      //   console.log(doc.id);
-      //   // console.log(doc.id, " => ", doc.data());
-      //   user_doc_id = doc.id;
-      // });
       .then((querySnapshot) => {
+        // usersのなかで、今ログインしている人と同じemailアドレスの人のidをuser_doc_idにリストアップ(使うのは一つだけ)
         querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, ' => ', doc.data());
-          //user_doc_id = doc.id;
           user_doc_id.push(doc.id);
-          console.log(
-            'この時点でログインしてる人のuser_doc_idは',
-            user_doc_id[0]
-          );
         });
-        console.log('今ログインしてる人のuser_doc_idは', user_doc_id[0]);
-        const bookmarkRef = db
-          .collection('bookmarks')
-          .where('user_doc_id', '==', user_doc_id[0]);
-        const snapshots = bookmarkRef.get();
-        snapshots.then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, ' => ', doc.data());
-            var novel_doc_id = doc.data().novel_doc_id;
-            const novelRef = db.collection('novels').doc(novel_doc_id);
-            novelRef.get().then((doc) => {
-              this.state.list.push(
-                <Article
-                  key={doc.id}
-                  title={doc.data().title}
-                  category={doc.data().category}
-                  author={doc.data().name}
-                  abstract={doc.data().overview}
-                  id={doc.id}
-                />
-              );
-              this.setState({ list: this.state.list });
+
+        // user_doc_idの一つ目の人のお気に入りの小説をリストアップ
+        db.collection('bookmarks')
+          .where('user_doc_id', '==', user_doc_id[0])
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              // お気に入りに登録されている小説IDから、その小説のデータを取得し、リストアップ
+              const favorite_novel_doc_id = doc.data().novel_doc_id;
+              db.collection('novels')
+                .doc(favorite_novel_doc_id)
+                .get()
+                .then((novel) => {
+                  if (novel.exists) {
+                    // 該当の小説があればfavoriteNovelsに追加
+                    this.state.favoriteNovels.push(
+                      <Article
+                        key={novel.id}
+                        title={novel.data().title}
+                        category={novel.data().category}
+                        author={novel.data().name}
+                        abstract={novel.data().overview}
+                        id={novel.id}
+                      />
+                    );
+                    this.setState({
+                      favoriteNovels: this.state.favoriteNovels,
+                    });
+                  } else {
+                    // 該当の小説がなければエラー出力
+                    console.log('Cannot find novel (in Bookmark)');
+                  }
+                });
             });
           });
-        });
       });
   }
   render() {
@@ -69,7 +63,7 @@ export default class extends React.Component {
       <div class="bookmark-page contents-list bookmark">
         <ScrollToTopOnMount />
         <h1>お気に入り</h1>
-        <div class="box-list-yaxis">{this.state.list}</div>
+        <div class="box-list-yaxis">{this.state.favoriteNovels}</div>
       </div>
     );
   }
