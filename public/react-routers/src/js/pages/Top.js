a7a821f3b5027d106542722457b2910c29f5f282
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-09-22 16:32:30 +0900

    change state name and add comments in Top

--- public/react-routers/src/js/pages/Top.js
+++ public/react-routers/src/js/pages/Top.js
@@ -2,76 +2,72 @@ import React from 'react';
 import Article from '../components/Article';
 import News from '../components/News';
 import ScrollToTopOnMount from '../components/ScrollToTopOnMount';
-import firebase, { db } from '../connectDB';
+import { db } from '../connectDB';
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       collapsed: true,
-      rating: '',
-      list: [],
-      list2: [],
-      list3: [],
+      rankings: [],
+      newNovals: [],
+      news: [],
     };
 
-    const novelRef = db
-      .collection('novels')
+    // ランキング上位十件を取得
+    db.collection('novels')
       .orderBy('rating', 'desc')
-      .limit(10);
-    const snapshots = novelRef.get();
-    snapshots.then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, ' => ', doc.data());
-        this.state.list.push(
-          <Article
-            key={doc.id}
-            title={doc.data().title}
-            rank={this.state.list.length + 1}
-            category={doc.data().category}
-            author={doc.data().name}
-            abstract={doc.data().overview}
-            id={doc.id}
-          />
-        );
-        this.setState({ list: this.state.list });
+      .limit(10)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.state.rankings.push(
+            <Article
+              key={doc.id}
+              title={doc.data().title}
+              rank={this.state.rankings.length + 1}
+              category={doc.data().category}
+              author={doc.data().name}
+              abstract={doc.data().overview}
+              id={doc.id}
+            />
+          );
+          this.setState({ rankings: this.state.rankings });
+        });
       });
-    });
 
-    const novelRef2 = db
-      .collection('novels')
+    // 新着小説上位十件を取得
+    db.collection('novels')
       .orderBy('created', 'desc')
-      .limit(10);
-    const snapshots2 = novelRef2.get();
-    snapshots2.then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, ' => ', doc.data());
-        this.state.list2.push(
-          <Article
-            key={doc.id}
-            title={doc.data().title}
-            category={doc.data().category}
-            author={doc.data().name}
-            abstract={doc.data().overview}
-            id={doc.id}
-          />
-        );
-        this.setState({ list2: this.state.list2 });
+      .limit(10)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.state.newNovals.push(
+            <Article
+              key={doc.id}
+              title={doc.data().title}
+              category={doc.data().category}
+              author={doc.data().name}
+              abstract={doc.data().overview}
+              id={doc.id}
+            />
+          );
+          this.setState({ newNovals: this.state.newNovals });
+        });
       });
-    });
-    const dbNews = db.collection('news');
-    dbNews.get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const title = doc.data().title;
-        this.state.list3.push(
-          <News key={doc.id} title={doc.data().title} id={doc.id} />
-        );
-        console.log(doc.id, ' => ', doc.data());
-        this.setState({ title: this.state.list3 });
+
+    // お知らせを取得
+    db.collection('news')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.state.news.push(
+            <News key={doc.id} title={doc.data().title} id={doc.id} />
+          );
+          this.setState({ news: this.state.news });
+        });
       });
-    });
   }
 
   render() {
@@ -80,15 +76,15 @@ export default class extends React.Component {
         <ScrollToTopOnMount />
         <div class="contents-list ranking">
           <h3>今日のランキング</h3>
-          <div class="box-list-yaxis">{this.state.list}</div>
+          <div class="box-list-yaxis">{this.state.rankings}</div>
         </div>
         <div class="contents-list new-novel">
           <h3>新着小説</h3>
-          <div class="box-list-yaxis">{this.state.list2}</div>
+          <div class="box-list-yaxis">{this.state.newNovals}</div>
         </div>
         <div class="contents-list news">
           <h3>お知らせ</h3>
-          <div class="box-list-yaxis">{this.state.list3}</div>
+          <div class="box-list-yaxis">{this.state.news}</div>
         </div>
       </div>
     );
