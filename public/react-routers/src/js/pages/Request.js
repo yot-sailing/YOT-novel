890517882384777bbd66bc173275c2d0a84615ce
mimimimizuki <mizuki0327.5@gmail.com>
2020-09-23 23:28:45 +0900

    odaibako created

--- public/react-routers/src/js/pages/Request.js
+++ public/react-routers/src/js/pages/Request.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import firebase, { db } from '../connectDB';
+import createNovel from './createNovel';
 
 export default class extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { request : []}
+    var user = firebase.auth().currentUser;
+    console.log(user)
+    var username = []
+    var user_id = []
+    db.collection('users').where('email', '==', user.email).get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        username.push(doc.data().username);
+        user_id.push(doc.id);
+      });
+      db.collection('odaibako').where('user', '==', user_id[0]).get().then((querySnapshot) =>{
+        querySnapshot.forEach((doc) => {
+          this.state.request.push(<div><p>{doc.data().request}</p><button onClick={() => this.props.history.push('/createNovel')}>書く</button></div>);
+          this.setState({request: this.state.request});
+        });
+      });
+    });
+  }
   render() {
-    return <div>ここに投稿されたお題を見るページを作成します。</div>;
+    return <div>
+      <div>{this.state.request}</div>
+      <button onClick={() => this.props.history.goBack()}>戻る</button>
+    </div>;
   }
 }
--- public/react-routers/src/js/pages/createRequest.js
+++ public/react-routers/src/js/pages/createRequest.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import firebase, { db } from '../connectDB';
 
 export default class extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      request: '',
+      user: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ request: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const request = this.state.request;
+    const user = this.state.user;
+    console.log(request)
+
+    
+    if (request == ""){
+      return;
+    }
+    if (this.state.user == '') {
+      return;
+    }
+    db.collection('odaibako').add({
+      user: user,
+      request_content : request
+    });
+    e.preventDefault();
+    alert('投稿しました.');
+    this.props.history.push("/mypage")
+  }
+  componentDidMount(e) {
+    const query = new URLSearchParams(this.props.location.search);
+    const author_id = query.get('id');
+    this.setState({user: author_id})
+  }
   render() {
-    return <div>ここにお題を投稿するページを作成します。</div>;
+    return <div>
+      ここにお題を投稿するページを作成します。<br />
+      <form onSubmit={this.handleSubmit}>
+      <textarea type="text"
+                id="request"
+                value={this.state.request}
+                cols='50'
+                rows='5'
+                onChange={this.handleChange}
+                placeholder="ここにお題箱の内容を書いてください"></textarea>
+      <br />
+      <button
+          type="submit"
+          className="waves-effect waves-light btn col">
+          投稿する
+      </button>
+      </form>
+      <button onClick={() => this.props.history.goBack()}>戻る</button>
+      </div>;
   }
 }
